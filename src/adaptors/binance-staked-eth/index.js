700988450201109c1faa35aed667818ebd4de90b
slasher125 <slasher125@protonmail.com>
2026-05-04 14:27:25 +0400

    fix

--- src/adaptors/binance-staked-eth/index.js
+++ src/adaptors/binance-staked-eth/index.js
@@ -54,7 +54,6 @@ const apy = async () => {
       searchTokenOverride: wbeth,
       apyBase: apr,
       tvlUsd: tvlBsc * ethPrice * wbethExchangeRate,
-      isIntrinsicSource: true,
     },
   ];
 };
